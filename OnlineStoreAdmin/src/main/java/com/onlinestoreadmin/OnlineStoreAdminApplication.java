1953c50c259f861b1a187226e134262dd765f603
srinivas ayachitula <srinivasayachitula@gmail.com>
2021-03-10 18:10:36 -0500

    Admin for Online Store

---
+++ OnlineStoreAdmin/src/main/java/com/onlinestoreadmin/OnlineStoreAdminApplication.java
@@ -0,0 +1,13 @@
+package com.onlinestoreadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreAdminApplication.class, args);
+	}
+
+}
---
+++ OnlineStoreAdmin/src/test/java/com/onlinestoreadmin/OnlineStoreAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinestoreadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
